65863d6f3d9cd82b9aa3599c4ea472e9084da8b3
Matthew Sheldon <msheldo@iu.edu>
2025-04-11 17:59:38 -0400

    Updated playermovement to add a basic jump feature
    Needs fixing

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+    public bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
+}
